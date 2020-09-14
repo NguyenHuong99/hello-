b92b687171d0f8daabd054501475fdf921140868
NguyenHuong99 <71129870+NguyenHuong99@users.noreply.github.com>
2020-09-14 14:46:32 +0700

    Add files via upload

---
+++ bai03.c
@@ -0,0 +1,16 @@
+#include<stdio.h> 
+#include<conio.h>
+main(){
+	int num;
+	 
+	printf("Nhap so num1 = 3\n");
+	printf("Nhap so num2 = 2\n");
+    printf("Tong = 5\n");
+    printf("Hieu = 1\n");
+    printf("Tich = 6\n");
+	printf("Thuong = 1.500000");
+	scanf("%d", &num); 
+	
+	printf("Binh phuong cua so do la: %d", num*num);
+	getch(); 
+} 
---
+++ bai06.c
@@ -0,0 +1,17 @@
+#include<stdio.h> 
+#include<conio.h>
+main(){
+	int Diemtoan = 10;
+	int Diemly = 10;
+	int Diemhoa = 10; 
+    int Tongdiem = 30; 
+	float Diemtrungbinh = 10;
+	
+	printf("Diemtoan = %d\n", Diemtoan);
+	printf("Diemly = %d\n", Diemly);
+	printf("Diemhoa = %d\n", Diemhoa);
+    printf("Tongdiem = %d\n", Tongdiem); 
+	printf("Diemtrungbinh = %.1f\n", Diemtrungbinh);
+	
+	getch(); 
+} 
