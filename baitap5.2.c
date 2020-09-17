b8d0ff81f2031d1a83a000c33e5098f0c0045cb9
NguyenHuong99 <71129870+NguyenHuong99@users.noreply.github.com>
2020-09-17 13:48:52 +0700

    lab3

---
+++ baitap5.2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	char line[80];
+	printf("Nhap vao ky tu: ", line);
+scanf("%[\nABCDEFGHIJKLMEDFSDFDSDSFSDS]", line);
+	fflush(stdin);
+	printf("Nhap vao ky tu: ", line);
+scanf("%[^\n]", line);
+
+	return 0;
+}
---
+++ lab3/vd6.7.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+int i;
+float x;
+char c;
+printf("Please enter\n");
+scanf("%3d %5f %c", &i, &x, &c);
+printf("\nThe values accepted are: %d, %f, %c,", i, x, c);
+
+
+
+
+	return 0;
+}
