2e358d9e2a8645e739bebfed34ffc8d5d6fbeb6e
NguyenHuong99 <71129870+NguyenHuong99@users.noreply.github.com>
2020-09-18 00:10:38 +0700

    lab1

---
+++ lab1/myprogamI.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <conio.h> 
+
+void main(){
+ 
+int x, y;
+char a = 'y';
+x = y = 0;
+if (a == 'y'){
+	x += 5;
+	printf("The numbers are %d and \t%d", x, y);
+} 
+ 
+} 
+
---
+++ lab1/vd4.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <conio.h> 
+
+main(){
+ char a, b, c;
+  int d, v, q; 
+
+     printf("\nHang ky tu : 'A'", a); 
+	 printf("\nTen dinh danh sai : $abc", b); 
+	 printf("\nTen dinh dang dung : _A", c); 
+     printf("\nHang so nguyen : 23", d); 
+     printf("\nDouble : 10.34", v); 
+	 printf("\nSo cham dong : 12112134.868686868686886", q);
+	  
+	return 0; 
+} 
+
+
