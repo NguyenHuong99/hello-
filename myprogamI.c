cefba397bf5236e6c34ef8844907f5ba16a3bbd5
NguyenHuong99 <71129870+NguyenHuong99@users.noreply.github.com>
2020-09-17 21:56:34 +0700

    rac
    rac

---
+++ myprogamI.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <conio.h> 
+
+main(){
+ int a, b, c, sum;
+ printf("\nEnter any three numbers: ");
+ scanf("%d %d %d", &a, &b, &c);
+ 
+ sum = a + b + c;
+ printf("\n Sum = %d", sum);
+  
+	return 0; 
+} 
+
---
+++ vd2.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <conio.h> 
+
+main(){
+int x, y;
+x = 5;
+y = 2;
+	printf("\n\nThe integer are : %d & %d", x, y);
+
+	printf("\n\nThe addition gives : %d", x + y);
+
+	printf("\n\nThe subtraction gives : %d", x - y);
+	
+	printf("\n\nThe multiplication gives : %d", x * y);
+
+	printf("\n\nThe division gives : %d", x / y);
+
+	printf("\n\nThe modulus gives : %d", x % y);
+
+	getchar(); 
+	
+
+	return 0; 
+} 
+
+
