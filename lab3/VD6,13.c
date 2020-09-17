5cc844b9d69bcddf796189ad605af87616eb5d18
NguyenHuong99 <71129870+NguyenHuong99@users.noreply.github.com>
2020-09-18 00:06:13 +0700

    lab3

---
+++ lab3/VD6,13.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+int breadth;
+float length, height;
+	scanf("%d%f%6.2f", &breadth, &length, &height); 
+	printf("%d %f %e", breadth, length, height);
+
+	return 0;
+}
---
+++ lab3/precede.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+int a = 10;
+float b = 24.67892345;
+char ch = 'A';
+printf("\nInterger data = %d", a);
+printf("\nFloat data = %f", b);
+printf("\nCharacter = %c", ch);
+printf("\nThis prints the string");
+printf("%s", "\nThis also prints a string"); 
+ 
+	
+
+	return 0;
+}
